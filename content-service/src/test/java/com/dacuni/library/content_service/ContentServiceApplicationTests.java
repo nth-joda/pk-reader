479264273aba1b0037ca8feed30934b02d66d5ff
HauNT <nthau.2k@gmail.com>
2025-06-24 12:42:36 +0700

    add content-service

---
+++ content-service/src/test/java/com/dacuni/library/content_service/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacuni.library.content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
